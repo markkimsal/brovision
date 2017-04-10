8852173d6ebce1c96aad8c033617c93627170067
Mark Kimsal <metrofindings@gmail.com>
2017-04-10 10:51:12 -0400

    Better apt messages

--- tasks/os/pkg-install-apt.sh
+++ tasks/os/pkg-install-apt.sh
@@ -1,9 +1,11 @@
 export DEBIAN_FRONTEND="noninteractive"
 if [ "$BRO_PKG_MGR_UPDATE" ];then
-apt-get update
+	bro_result_pass "apt-get update ..."
+	apt-get update
 fi
 
 for pkg in "${BRO_PACKAGES[@]}"
 do
-	apt-get install -y $pkg
+	bro_result_pass "installing $pkg ..."
+	apt-get install -y --force-yes $pkg
 done
